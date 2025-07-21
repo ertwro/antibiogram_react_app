1d89738506c2364c1c7ab0c02c3e46a216d5897b
ertwro <ertwro@gmail.com>
2025-07-20 22:29:55 -0500

    build: Update generated indexes after rebuild

--- src/data/generatedAntibioticsIndex.js
+++ src/data/generatedAntibioticsIndex.js
@@ -24251,7 +24251,7 @@ export const databaseStats = {
   STREPTOGRAMIN: 2,
   TETRACYCLINE: 6
   },
-  lastGenerated: '2025-07-21T03:04:16.948Z'
+  lastGenerated: '2025-07-21T03:28:47.340Z'
 };
 
 // Helper functions
